1556941ad5cd49e6336eeb6b2b13e3bdd54ccddc
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-11 07:57:04 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,23 @@ import 'src/imports/packages_imports.dart';
 import 'src/app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
   FlutterNativeSplash.preserve(
-    widgetsBinding: WidgetsBinding.instance,
+    widgetsBinding: widgetsBinding,
   );
 
-  try {
-    await EasyLocalization.ensureInitialized();
-    await dotenv.load(fileName: '.env');
-    await AppConfig.init();
-    await HiveService.instance.init();
+  await EasyLocalization.ensureInitialized();
+  await AppConfig.init();
+  await HiveService.instance.init();
 
-    runApp(
-      const LocalizationWrapper(
-        child: StateWrapper(
-          child: App(),
-        ),
+  runApp(
+    const LocalizationWrapper(
+      child: StateWrapper(
+        child: App(),
       ),
-    );
-  } catch (e, stackTrace) {
-    debugPrint('APP STARTUP ERROR: $e');
-    debugPrintStack(stackTrace: stackTrace);
+    ),
+  );
 
-    runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Text('Startup Error:\n$e'),
-          ),
-        ),
-      ),
-    );
-  } finally {
-    FlutterNativeSplash.remove();
-  }
+  FlutterNativeSplash.remove();
 }
