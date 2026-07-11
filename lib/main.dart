0f3550e61cbc9c6a97a4b6ab51c2e163042e1a2e
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-11 18:36:45 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'src/imports/core_imports.dart';
 import 'src/imports/packages_imports.dart';
 import 'src/app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
-  await EasyLocalization.ensureInitialized();
+  try {
+    await EasyLocalization.ensureInitialized();
+    await dotenv.load(fileName: '.env');
+    await AppConfig.init();
+    await HiveService.instance.init();
+  } catch (e, s) {
+    debugPrint('Startup Error: $e');
+    debugPrintStack(stackTrace: s);
+  }
 
-  await dotenv.load(
-    fileName: '.env',
-  );
-
-  await AppConfig.init();
-
-  await HiveService.instance.init();
+  FlutterNativeSplash.remove();
 
   runApp(
     const LocalizationWrapper(
