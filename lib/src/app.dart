c72ec3537dc777a5443a7ee6827c4ba5f52f1c03
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-24 16:20:48 +0100

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,22 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ztc_bank/src/imports/core_imports.dart';
 
-class App extends StatelessWidget {
+class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final current = _buildMaterialApp(context);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = buildRouter(ref);
+    
+    final current = _buildMaterialApp(context, appRouter);
     return ScreenUtilWrapper(child: current);
   }
 
-  Widget _buildMaterialApp(BuildContext context) {
+  Widget _buildMaterialApp(BuildContext context, GoRouter router) {
     return MaterialApp.router(
       title: 'ztc_bank',
       debugShowCheckedModeBanner: false,
       theme: buildLightTheme(primaryColorHex: '#6750A4'),
       darkTheme: buildDarkTheme(primaryColorHex: '#6750A4'),
       themeMode: ThemeMode.system,
-      routerConfig: appRouter,
+      routerConfig: router,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
