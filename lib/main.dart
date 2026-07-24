b79d98a517ca1224db4575668c6029e47be99597
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-24 08:57:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'src/imports/core_imports.dart';
-import 'src/imports/packages_imports.dart';
 import 'src/app.dart';
 
 Future<void> main() async {
-  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
 
-  try {
-    await EasyLocalization.ensureInitialized();
-    await dotenv.load(fileName: '.env');
-    await AppConfig.init();
-    await HiveService.instance.init();
-  } catch (e, s) {
-    debugPrint('Startup Error: $e');
-    debugPrintStack(stackTrace: s);
-  }
+  await EasyLocalization.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.env["SUPABASE_URL"]!,
+    anonKey: dotenv.env["SUPABASE_ANON_KEY"]!,
+  );
+
+  await AppConfig.init();
+  await HiveService.instance.init();
 
   FlutterNativeSplash.remove();
 
   runApp(
     EasyLocalization(
-      supportedLocales: const [Locale('en')],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('en'),
-      child: const StateWrapper(
-        child: App(),
-      ),
+      supportedLocales: const [Locale("en")],
+      path: "assets/translations",
+      fallbackLocale: const Locale("en"),
+      child: const StateWrapper(child: App()),
     ),
   );
 }
