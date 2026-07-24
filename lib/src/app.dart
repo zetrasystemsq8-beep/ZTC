c0f39a744958561da3b18ea49aaa51dd4bf97ce2
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-24 17:04:02 +0100

    Update app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -8,8 +8,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ztc_bank/src/imports/core_imports.dart';
 import 'package:ztc_bank/src/imports/packages_imports.dart';
 import 'package:ztc_bank/src/routing/app_router.dart';
-import 'package:ztc_bank/src/theme/app_theme.dart';
-import 'package:ztc_bank/src/widgets/skeleton_wrapper.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -26,18 +24,13 @@ class App extends ConsumerWidget {
     return MaterialApp.router(
       title: 'ztc_bank',
       debugShowCheckedModeBanner: false,
-      theme: buildLightTheme(primaryColorHex: '#6750A4'),
-      darkTheme: buildDarkTheme(primaryColorHex: '#6750A4'),
+      theme: ThemeData(useMaterial3: true),
+      darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
       themeMode: ThemeMode.system,
       routerConfig: router,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
-      builder: (context, child) {
-        Widget current = child!;
-        current = SkeletonWrapper(child: current);
-        return current;
-      },
     );
   }
 }
