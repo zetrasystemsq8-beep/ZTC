6882d2276aef7413ea361c997d63c820d4dd719d
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-24 09:46:10 +0100

    Update app_router.dart

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+
 import 'package:ztc_bank/src/routing/global_navigator.dart';
 import 'package:ztc_bank/src/routing/app_routes.dart';
 
@@ -8,71 +9,124 @@ import 'package:ztc_bank/src/features/auth/presentation/screens/forgot_password_
 
 import 'package:ztc_bank/src/features/home/presentation/screens/home_page.dart';
 import 'package:ztc_bank/src/features/onboarding/presentation/screens/onboarding_page.dart';
+
 import 'package:ztc_bank/src/features/wallet/presentation/screens/wallet_home.dart';
+
 import 'package:ztc_bank/src/features/transactions/presentation/screens/transactions_list_screen.dart';
 import 'package:ztc_bank/src/features/transactions/presentation/screens/transaction_detail_screen.dart';
+
 import 'package:ztc_bank/src/features/send_receive/presentation/screens/send_money_screen.dart';
 import 'package:ztc_bank/src/features/send_receive/presentation/screens/receive_money_screen.dart';
+
+import 'package:ztc_bank/src/features/payments/presentation/screens/payments_screen.dart';
+import 'package:ztc_bank/src/features/cards/presentation/screens/cards_screen.dart';
+import 'package:ztc_bank/src/features/account/presentation/screens/account_screen.dart';
+
 
 final GoRouter appRouter = GoRouter(
   navigatorKey: rootNavigatorKey,
 
-  // Developer mode: skip onboarding/login and open Home directly.
+  // Developer mode: skip onboarding/login
   initialLocation: AppRoutes.home,
 
   routes: <RouteBase>[
+
     GoRoute(
       path: AppRoutes.onboarding,
       name: 'onboarding',
-      builder: (context, state) => const OnboardingPage(),
+      builder: (context, state) =>
+          const OnboardingPage(),
     ),
+
     GoRoute(
       path: AppRoutes.login,
       name: 'login',
-      builder: (context, state) => const LoginScreen(),
+      builder: (context, state) =>
+          const LoginScreen(),
     ),
+
     GoRoute(
       path: AppRoutes.signup,
       name: 'signup',
-      builder: (context, state) => const SignupScreen(),
+      builder: (context, state) =>
+          const SignupScreen(),
     ),
+
     GoRoute(
       path: AppRoutes.forgotPassword,
       name: 'forgotPassword',
-      builder: (context, state) => const ForgotPasswordScreen(),
+      builder: (context, state) =>
+          const ForgotPasswordScreen(),
     ),
+
     GoRoute(
       path: AppRoutes.home,
       name: 'home',
-      builder: (context, state) => const HomePage(),
+      builder: (context, state) =>
+          const HomePage(),
     ),
+
     GoRoute(
       path: AppRoutes.wallet,
       name: 'wallet',
-      builder: (context, state) => const WalletHome(),
+      builder: (context, state) =>
+          const WalletHome(),
     ),
+
     GoRoute(
       path: AppRoutes.transactions,
       name: 'transactions',
-      builder: (context, state) => const TransactionsListScreen(),
+      builder: (context, state) =>
+          const TransactionsListScreen(),
     ),
+
     GoRoute(
       path: '${AppRoutes.transactions}/:id',
       name: 'transactionDetail',
       builder: (context, state) {
         final id = state.pathParameters['id']!;
-        return TransactionDetailScreen(transactionId: id);
+        return TransactionDetailScreen(
+          transactionId: id,
+        );
       },
     ),
+
     GoRoute(
       path: AppRoutes.sendMoney,
       name: 'sendMoney',
-      builder: (context, state) => const SendMoneyScreen(),
+      builder: (context, state) =>
+          const SendMoneyScreen(),
     ),
+
     GoRoute(
       path: AppRoutes.receiveMoney,
       name: 'receiveMoney',
-      builder: (context, state) => const ReceiveMoneyScreen(),
+      builder: (context, state) =>
+          const ReceiveMoneyScreen(),
+    ),
+
+
+    // New grouped features
+
+    GoRoute(
+      path: '/payments',
+      name: 'payments',
+      builder: (context, state) =>
+          const PaymentsScreen(),
+    ),
+
+    GoRoute(
+      path: '/cards',
+      name: 'cards',
+      builder: (context, state) =>
+          const CardsScreen(),
+    ),
+
+    GoRoute(
+      path: '/account',
+      name: 'account',
+      builder: (context, state) =>
+          const AccountScreen(),
     ),
   ],
 );
