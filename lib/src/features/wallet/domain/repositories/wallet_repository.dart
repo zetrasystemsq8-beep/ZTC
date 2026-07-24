9f49aa8957f2f2af9934a0107780a1b156fd920a
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-24 11:07:09 +0100

    Update wallet_repository.dart

--- lib/src/features/wallet/domain/repositories/wallet_repository.dart
+++ lib/src/features/wallet/domain/repositories/wallet_repository.dart
@@ -4,25 +4,25 @@ import 'package:ztc_bank/src/features/wallet/domain/entities/transaction.dart';
 
 abstract class WalletRepository {
   FutureEither<Wallet> getWallet();
-
+  
   FutureEither<List<Transaction>> getRecentTransactions({int limit = 10});
-
+  
   FutureEither<Transaction> deposit({
     required double amount,
     required String description,
   });
-
+  
   FutureEither<Transaction> withdraw({
     required double amount,
     required String description,
   });
-
+  
   FutureEither<Transaction> send({
     required double amount,
     required String recipientEmail,
     required String description,
   });
-
+  
   FutureEither<Transaction> receive({
     required double amount,
     required String description,
