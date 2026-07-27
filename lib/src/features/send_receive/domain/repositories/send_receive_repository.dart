b9f51a13556e28e665a3af4608b9e598801f0b75
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-27 11:32:19 +0100

    Update send_receive_repository.dart

--- lib/src/features/send_receive/domain/repositories/send_receive_repository.dart
+++ lib/src/features/send_receive/domain/repositories/send_receive_repository.dart
@@ -1,19 +1,12 @@
+import 'package:ztc_bank/src/utils/utils.dart';
 import 'package:ztc_bank/src/features/send_receive/domain/entities/user.dart';
 import 'package:ztc_bank/src/features/wallet/domain/entities/transaction.dart';
-import 'package:ztc_bank/src/utils/typedefs.dart';
 
 abstract class SendReceiveRepository {
-  FutureEither<List<User>> searchUsers({
-    required String query,
-  });
-
+  FutureEither<List<User>> searchUsers({required String query});
   FutureEither<User> getUserByEmail(String email);
-
   FutureEither<User> getUserById(String id);
-
-  FutureEither<List<User>> getRecentRecipients({
-    int limit = 10,
-  });
+  FutureEither<List<User>> getRecentRecipients({int limit = 10});
 
   FutureEither<Transaction> transfer({
     required String recipientId,
@@ -28,8 +21,5 @@ abstract class SendReceiveRepository {
   });
 
   FutureEither<String> generateReceiveQRCode();
-
-  FutureEither<Map<String, dynamic>> parseQRCode(
-    String qrData,
-  );
+  FutureEither<Map<String, dynamic>> parseQRCode(String qrData);
 }
