9218f212e11fa02992dd91f5ee3f70631ec27bed
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-27 11:40:25 +0100

    Update send_receive_repository_impl.dart

--- lib/src/features/send_receive/data/repositories/send_receive_repository_impl.dart
+++ lib/src/features/send_receive/data/repositories/send_receive_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:ztc_bank/src/utils/utils.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:supabase_flutter/supabase_flutter.dart' hide User;
 import 'package:ztc_bank/src/features/send_receive/domain/entities/user.dart';
 import 'package:ztc_bank/src/features/send_receive/domain/repositories/send_receive_repository.dart';
 import 'package:ztc_bank/src/features/wallet/data/models/transaction_model.dart';
