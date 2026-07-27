4697e887537adc094a19e82242f3e84662b98092
Oyedele Toluwani <zetra.systems.q8@gmail.com>
2026-07-27 11:34:11 +0100

    Update user.dart

--- lib/src/features/send_receive/domain/entities/user.dart
+++ lib/src/features/send_receive/domain/entities/user.dart
@@ -4,52 +4,27 @@ class User extends Equatable {
   final String id;
   final String email;
   final String? name;
+  final String? zetraId;
   final String? photoUrl;
-  final String? phoneNumber;
-  final bool isVerified;
 
   const User({
     required this.id,
     required this.email,
     this.name,
+    this.zetraId,
     this.photoUrl,
-    this.phoneNumber,
-    this.isVerified = false,
   });
 
-  @override
-  List<Object?> get props => [id, email, name, photoUrl, phoneNumber, isVerified];
-}
-
-class UserModel extends User {
-  const UserModel({
-    required super.id,
-    required super.email,
-    super.name,
-    super.photoUrl,
-    super.phoneNumber,
-    super.isVerified,
-  });
-
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'] as String? ?? '',
-      email: json['email'] as String? ?? '',
-      name: json['name'] as String?,
-      photoUrl: json['photoUrl'] as String?,
-      phoneNumber: json['phoneNumber'] as String?,
-      isVerified: json['isVerified'] as bool? ?? false,
+  factory User.fromProfileRow(Map<String, dynamic> row) {
+    return User(
+      id: row['id'] as String,
+      email: row['email'] as String? ?? row['zetramail'] as String? ?? '',
+      name: row['full_name'] as String? ?? row['username'] as String?,
+      zetraId: row['zetra_id'] as String?,
+      photoUrl: row['avatar_url'] as String?,
     );
   }
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'email': email,
-      'name': name,
-      'photoUrl': photoUrl,
-      'phoneNumber': phoneNumber,
-      'isVerified': isVerified,
-    };
-  }
+  @override
+  List<Object?> get props => [id, email, name, zetraId, photoUrl];
 }
